08c06f5b2f11135f05d7250a8ffdb5d73316193d
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-03-20 12:11:58 +0100

    Add active menu selection (non functional)

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
 import './navigation.scss'
 
 function Navigation() {
+  const [active, setActive] = useState(0)
+
+  const handleClick = (activeIndex: number) => {
+    setActive(activeIndex)
+  }
+
+  const isActiveClass = (index: number): string => {
+    return active === index ? 'is-active' : ''
+  }
+
   return (
     <nav className="navigation">
-      <ul className="items">
-        <li><a className="is-active item">/</a></li>
-        <li><a className="item">Music</a></li>
-        <li><a className="item">CV</a></li>
-        <li><a className="item">About</a></li>
+      <ul className="menu">
+        <li><a className={`${isActiveClass(0)} item`} onClick={() => handleClick(0)}>/</a></li>
+        <li><a className={`${isActiveClass(1)} item`} onClick={() => handleClick(1)}>Music</a></li>
+        <li><a className={`${isActiveClass(2)} item`} onClick={() => handleClick(2)}>CV</a></li>
+        <li><a className={`${isActiveClass(3)} item`} onClick={() => handleClick(3)}>About</a></li>
       </ul>
     </nav>
   )
