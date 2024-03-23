213d6173d518b0ab2e49f3dd648bc22badf781b8
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-03-23 16:32:26 +0100

    Set title font, refactor footer classes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ function Footer() {
     <footer className="footer has-background-info">
       <div className="content has-text-centered">
         © Al-Mudawwar {new Date().getFullYear()}
-        <div className="is-flex is-flex-direction-row is-justify-content-center mt-2">
+        <div className="is-flex is-flex-direction-row is-justify-content-center mt-3 is-gap-2">
           {
             externalLinks.map(site => {
-              return <div className="mr-2 ml-2" key={site.name}>
+              return <div key={site.name}>
               <a href={site.url} target='_blank'>
                 <img src={site.logoPath} className='logo' alt={site.name} />
               </a>
