5f6df6080cc142243c3626db01f0a0d665c8162f
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-03-25 12:30:14 +0100

    Remove skills from homepage

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,6 @@
-import { SKILLS } from '../../constants/skills'
 import './hero.scss'
 import TileGroup from '../TileGroup'
 import TileItem from '../TileItem'
-import Tag from '../Tag'
 import { releases } from '../../constants/releases'
 
 function Hero() {
@@ -14,7 +12,7 @@ function Hero() {
     <section className="hero is-fullheight mt-5">
       <div className="hero-body ">
       <TileGroup>
-        <TileItem title='Al-Mudawwar' subtitle={`“that which is round”`} />
+        <TileItem title='Al-Mudawwar' subtitle={`“that which is round”`} rows={2} />
 
         <TileItem dark title="Music to daydream to" rows={2} linkTo="/music">
           <figure className="image is-1by1">
@@ -22,15 +20,7 @@ function Hero() {
           </figure>
         </TileItem>
         
-        <TileItem dark title="Software Development" rows={2} linkTo="/links">
-          <div className="tags">
-            {
-              SKILLS.map(skill => <Tag key={skill} text={skill} bold dark rounded />)
-            }
-          </div>
-        </TileItem>
-
-        <TileItem dark subtitle="...and a musical project by" linkTo="/about">
+        <TileItem dark subtitle="...and a musical project by" linkTo="/about" rows={2}>
           <p><span className="tag is-primary is-large is-rounded">Rodrigo Guerreiro</span></p>
         </TileItem>
         
