114aea21e2a009a0e23fb755fb941343f84ec4bc
Rodrigo <rodrigopg8@gmail.com>
2024-03-27 12:36:23 +0100

    New home page design (#8)
    * Add new homepage design
    
    * Add circle

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,32 +1,12 @@
 import './hero.scss'
-import TileGroup from '../TileGroup'
-import TileItem from '../TileItem'
-import { releases } from '../../constants/releases'
+import PageHeader from '../PageHeader/PageHeader'
 
 function Hero() {
-  const getReleaseCover = (): string => {
-    return releases[Math.floor(Math.random() * releases.length)].imageUrl
-  }
-
   return (
-    <section className="hero is-fullheight mt-5">
-      <div className="hero-body ">
-      <TileGroup>
-        <TileItem title='Al-Mudawwar' subtitle={`“that which is round”`} rows={2} />
-
-        <TileItem dark title="Music to daydream to" rows={2} linkTo="/music">
-          <figure className="image is-1by1">
-            <img src={getReleaseCover()} />
-          </figure>
-        </TileItem>
-        
-        <TileItem dark subtitle="...a musical project by" linkTo="/about" rows={2}>
-          <p><span className="tag is-primary is-large is-rounded">Rodrigo Guerreiro</span></p>
-        </TileItem>
-        
-      </TileGroup>
-      </div>
-    </section>
+    <div className='homepage-wrapper'>
+      <div className='circle'></div>
+      <PageHeader title='Al-Mudawwar' />
+    </div>
   )
 }
 
