ad3fc57c4e8a1644c9f45b6ae87b9fc8e49977e7
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-03-27 12:29:06 +0100

    Add circle

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,6 +4,7 @@ import PageHeader from '../PageHeader/PageHeader'
 function Hero() {
   return (
     <div className='homepage-wrapper'>
+      <div className='circle'></div>
       <PageHeader title='Al-Mudawwar' />
     </div>
   )
