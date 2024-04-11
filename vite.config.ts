3686f858e279e229bbf280dfcbca1626c39f0d76
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-04-11 12:32:12 +0200

    Add vite-plugin-vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
   css: {
     preprocessorOptions: {
       scss: {
