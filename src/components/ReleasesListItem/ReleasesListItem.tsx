ed564bb9610ed2fb53568ae12fa63ac9d4da39ec
Rodrigo Guerreiro <rodrigopg8@gmail.com>
2024-04-11 09:50:26 +0200

    Add flip button to see release tracklist

--- src/components/ReleasesListItem/ReleasesListItem.tsx
+++ src/components/ReleasesListItem/ReleasesListItem.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react"
+import { GiSideswipe } from "react-icons/gi";
 import { Release } from "../../@types/release"
 import Button from "../Button"
 import './releasesListItem.scss'
-import PageHeader from "../PageHeader/PageHeader"
 
 interface ReleasesListItemProps {
   release: Release
@@ -16,37 +16,35 @@ function ReleasesListItem({ release }: ReleasesListItemProps) {
   }
 
   const renderedBack = () => {
+    const columnClass = release.tracklist.length > 3 ? 'long' : ''
     return (
-      <div className="is-flex is-flex-direction-column is-flex-grow-1 is-justify-content-space-evenly p-4">
-        <span className="title">Title</span>
-        <ol className="tracklist">
-          <li>Song 1</li>
-          <li>Roots, Plants, Galaxies</li>
-          <li>Song 3</li>
-          <li>Song 4</li>
-          <li>Song 5</li>
-          <li>Song 6</li>
-          <li>Song 7</li>
-          <li>Song 8</li>
-          <li>Song 9</li>
+      <div className="release-back is-flex is-flex-direction-column is-flex-grow-1 is-justify-content-space-evenly p-4">
+        <span className="title">{release.name}</span>
+        <ol className={`tracklist ${columnClass}`}>
+          {
+            release.tracklist.map(track => <li>{track}</li>)
+          }
         </ol>
         <div className="buttons is-justify-content-center">
           <Button text="Listen" rounded dark linkTo="/links" />
-          <Button text="Buy" rounded primary />
+          <Button text="Buy" rounded primary linkTo={release.bandcampUrl} />
         </div>
       </div>
     )
   }
 
+  const columnClass = isFlipped ? 'flipped' : ''
+
   return (
-    <div className="column">
-      <div className="release-container is-flex">
-        {
-          isFlipped ? renderedBack() : <img src={release.imageUrl} />
-        }
+    <div className="column is-relative">
+      <div className={`release-container is-flex ${columnClass}`}>
+        <img className="release-front" src={release.imageUrl} />
+        {renderedBack()}
       </div>
       
-      <Button text="Flip me!" rounded onClick={handleClick} />
+      <Button className="flip-button is-position-absolute" rounded onClick={handleClick}>
+        <GiSideswipe />
+      </Button>
     </div>
   )
 }
--- src/constants/releases.ts
+++ src/constants/releases.ts
@@ -4,25 +4,47 @@ export const releases: Release[] = [
   {
     name: 'Natureza Sagrada',
     imageUrl: 'https://f4.bcbits.com/img/a1408437784_10.jpg',
+    bandcampUrl: 'https://al-mudawwar.bandcamp.com/album/natureza-sagrada',
+    tracklist: [
+      'Entrada',
+      'Roots, Plants, Galaxies',
+      'Daydreaming',
+      'Bilhete Para a Lua',
+      'Sinking Into the Universe',
+      'Daydreaming II',
+      'Healing',
+      'Medicina',
+      'Dancing Trees',
+    ]
   },
   {
     name: 'Primeira Neve',
     imageUrl: 'https://f4.bcbits.com/img/a2011955226_10.jpg',
+    bandcampUrl: 'https://al-mudawwar.bandcamp.com/track/primeira-neve',
+    tracklist: ['Primeira Neve']
   },
   {
     name: 'Every Thing You See',
     imageUrl: 'https://f4.bcbits.com/img/a3353901797_10.jpg',
+    bandcampUrl: 'https://al-mudawwar.bandcamp.com/track/every-thing-you-see',
+    tracklist: ['Every Thing You See']
   },
   {
     name: 'Chilaquiles',
     imageUrl: 'https://f4.bcbits.com/img/a0573872708_10.jpg',
+    bandcampUrl: 'https://al-mudawwar.bandcamp.com/track/chilaquiles',
+    tracklist: ['Chilaquiles']
   },
   {
     name: 'Friends',
     imageUrl: 'https://f4.bcbits.com/img/a1328975379_10.jpg',
+    bandcampUrl: 'https://al-mudawwar.bandcamp.com/album/friends',
+    tracklist: ['Friends', 'The Birds and the Wind']
   },
   {
     name: 'A Brief Hiatus',
     imageUrl: 'https://f4.bcbits.com/img/a1271692749_10.jpg',
+    bandcampUrl: 'https://al-mudawwar.bandcamp.com/track/a-brief-hiatus',
+    tracklist: ['A Brief Hiatus']
   }
 ]
